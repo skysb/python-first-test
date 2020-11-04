87522bd1ff9e84efb4ca8f107bff96d58923d3e7
spbelani <spbelani@alintia.com>
2020-11-04 14:38:39 +0100

    thanos mode squared

--- file1.py
+++ file1.py
@@ -1,6 +1,11 @@
 import folder.file2
 import folder.file2 as file2Alias
 from folder.file2 import File2Class, File2Class2 as Pepino
+from folder.file2 import outside_call
+from folder.file2 import outside_call as outsidecallalias
+
+outside_call()
+outsidecallalias()
 
 file2Alias.File2Class.print_file2_class()
 Pepino.print_file2_class()
@@ -13,15 +18,20 @@ file2Alias.outside_call()
 
 class File1Class:
 
+    outside_call()
+    outsidecallalias()
     file2Alias.File2Class.print_file2_class()
-
+    file2Alias.outside_call()
+    File2Class.print_file2_class()
+    
     def file1method():
+        
+        outside_call()
+        outsidecallalias()
         File2Class.print_file2_class()
         file2Alias.outside_call()
         file2Alias.File2Class.print_file2_class()
 
 
-    file2Alias.outside_call()
-    File2Class.print_file2_class()
 
     
